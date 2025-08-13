e73592a1c8c4538017d55faa916ad4c0b0f39d61
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-13 08:38:01 -0300

    Initial commit

---
+++ src/test/java/br/com/fiap3espf/springbootproject/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap3espf.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
